7ea9bb3cd456335e39ecf53edf511e1736323981
unknown <Dmitry@.(none)>
2009-12-12 01:59:13 +0200

    Licensing info update

--- src/js/dep/jQuery/fileUploader.dependency.jQuery.js
+++ src/js/dep/jQuery/fileUploader.dependency.jQuery.js
@@ -2,6 +2,23 @@
   * Author: Dmitry Stepanov
   * E-Mail: dmitrij@stepanov.lv
   * URL: http://www.stepanov.lv
+  
+  File Uploader - parallel asynchronous file uploader.
+  Copyright (C) 2009  Dmitry Stepanov <dmitrij@stepanov.lv>
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
   */
 
 (function( $, api){
