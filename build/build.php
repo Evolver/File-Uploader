3ac719feb2d2a1995ca99cc057dc295a4b3ec8c2
Dmitry Stepanov <dmitrij@stepanov.lv>
2010-06-03 15:46:17 +0300

    Updated build script.

--- build/build.php
+++ build/build.php
@@ -4,7 +4,7 @@
 // Mon Dec 14 10:02:20 GMT 2009 10:02:20 build.php
 
 function usage() {
-  echo 'Usage: php build.php <source directory> <dependent library name> <output file name>' ."\n";
+  echo 'Usage: php build.php <dependent library name>' ."\n";
   die();
 }
 
@@ -17,45 +17,50 @@ function write_error( $filePath) {
   echo 'Unable to write to "' .$filePath .'"' ."\n";
   die();
 }
-
-// Builds jQuery bundle
-if( !isset( $argv))
-  die( 'register_argc_argv is not enabled');
   
-if( count( $argv) <3)
+if( count( $argv) <2)
   usage();
   
 // get dependency name
-$sourceRoot =$argv[1];
-$dependency =$argv[2];
-$outputPath =$argv[3];
+$sourceRoot =dirname( __DIR__) .'/src/';
+$dependency =$argv[1];
+$outputPath =__DIR__ .'/fileUploader.js';
 
 // build bundle
-if(( $dependencyJs =file_get_contents( $filePath =$sourceRoot .'/src/js/dep/' .$dependency .'/fileUploader.' .$dependency .'.js')) ===false)
+if(( $dependencyJs =file_get_contents( $filePath =$sourceRoot .'js/dep/' .$dependency .'/fileUploader.' .$dependency .'.js')) ===false)
   read_error( $filePath);
   
-if(( $coreJs =file_get_contents( $filePath =$sourceRoot .'/src/js/fileUploader.core.js')) ===false)
+if(( $coreJs =file_get_contents( $filePath =$sourceRoot .'js/fileUploader.core.js')) ===false)
   read_error( $filePath);
   
-if(( $apiJs =file_get_contents( $filePath =$sourceRoot .'/src/js/api/fileUploader.Uploader.js')) ===false)
+if(( $apiJs =file_get_contents( $filePath =$sourceRoot .'js/api/fileUploader.Uploader.js')) ===false)
   read_error( $filePath);
   
-if(( $dependencyApiJs =file_get_contents( $filePath =$sourceRoot .'/src/js/api/' .$dependency .'/fileUploader.' .$dependency .'.js')) ===false)
+if(( $dependencyApiJs =file_get_contents( $filePath =$sourceRoot .'js/api/' .$dependency .'/fileUploader.' .$dependency .'.js')) ===false)
   read_error( $filePath);
   
-// build output code
-$output =
-  '// File-Uploader ' .$dependency .' API, build ' .date( 'Y/m/d H:i:s') .'. Get your own File Uploader at http://github.com/Evolver/File-Uploader' ."\n\n" .
-  '// ' .'src/js/dep/' .$dependency .'/fileUploader.' .$dependency .'.js' ."\n\n" .$dependencyJs ."\n\n" .
-  '// ' .'src/js/fileUploader.core.js' ."\n\n" .$coreJs ."\n\n" .
-  '// ' .'src/js/api/fileUploader.Uploader.js' ."\n\n" .$apiJs ."\n\n" .
-  '// ' .'src/js/api/' .$dependency .'/fileUploader.' .$dependency .'.js' ."\n\n" .$dependencyApiJs;
-  
+// build output
+$output =<<<COPYRIGHT_NOTICE
+/**
+ * File-Uploader - Evolver's file uploading script.
+ * http://github.com/Evolver/File-Uploader
+ *
+ * Copyright (C) 2010 Dmitry Stepanov <dmitrij@stepanov.lv>
+ * URL: http://www.stepanov.lv
+ *
+ * Publicly available for non-commercial use under GPL v2 license terms.
+ */
+ 
+COPYRIGHT_NOTICE;
+
+// append output with script
+$output .=$dependencyJs;
+$output .=$coreJs;
+$output .=$apiJs;
+$output .=$dependencyApiJs;
   
 // output bundled file
 if( !file_put_contents( $outputPath, $output))
   write_error( $outputPath);
   
-echo 'Written to "' .$outputPath .'". If you wish to compress output file to a smaller size, use http://javascriptcompressor.com/ online tool to compress and obfuscate JavaScript code' ."\n";
-
 ?>
